d989836e6d9d49bb479a9e89f7faa6617043b0fb
yehpop <smsglxyn8@gmail.com>
2025-02-08 20:36:00 +0300

    Start out on Elevator

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -4,15 +4,68 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionDutyCycle;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElevatorSubsystem extends SubsystemBase {
-  private final int motor = 1;
+  // Kraken X60
+  private final TalonFX m_motor = new TalonFX(10);
+
+  private final DutyCycleOut m_motorOut = new DutyCycleOut(0);
+  private final VoltageOut m_motorVoltage = new VoltageOut(0);
+  // Position unit???
+  private final PositionVoltage m_positionVoltageControl = new PositionVoltage(0);
+  private final PositionDutyCycle m_positionControl = new PositionDutyCycle(0);
+
+  private final TalonFXConfiguration m_motorConfig = new TalonFXConfiguration();
+  
   /** Creates a new ElevatorSubsystem. */
   public ElevatorSubsystem() {
+    var slot0motorConfigs = m_motorConfig.Slot0;
+    slot0motorConfigs.withGravityType(GravityTypeValue.Elevator_Static);
+    slot0motorConfigs.kV = 0;
+    slot0motorConfigs.kP = 0;
+    slot0motorConfigs.kI = 0;
+    slot0motorConfigs.kD = 0;
+
+    // Apply configs
+    m_motor.getConfigurator().apply(slot0motorConfigs, 0.05);
 
   }
   
+  /**
+   * Incomplete.
+   * Learn details on position unit and conversion and return to writing.
+   * 
+   * @param position
+   * @return
+   */
+  public Command setPositionWithVoltComp(double position) {
+    m_positionVoltageControl.Slot = 0;
+    return run(() -> m_motor.setControl(m_positionVoltageControl.withPosition(position)));
+  }
+
+  /**
+   * Incomplete.
+   * Learn details on position unit and conversion and return to writing.
+   * 
+   * @param position
+   * @return
+   */
+  public Command setPosition(double position) {
+    m_positionControl.Slot = 0;
+    return run(() -> m_motor.setControl(m_positionControl.withPosition(position)));
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
