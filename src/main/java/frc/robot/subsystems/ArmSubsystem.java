c233510fd5afd79d949b9eab8af0c13a9f25e9ba
yehpop <smsglxyn8@gmail.com>
2025-02-10 21:13:15 +0300

    Start programming arm

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,11 +4,25 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * Double-Jointed Arm.
+ * Two Krakens move the joints from a gearbox at the elevator.
+ * 
+ * Might be complicated to program, use magic motion position.
+ */
 public class ArmSubsystem extends SubsystemBase {
-  private final int motor_shoulder = 0;
-  private final int motor_elbow = 1;
+  private final TalonFX m_motorShoulder = new TalonFX(12);
+  private final TalonFX m_motorElbow = new TalonFX(13);
+
+  private final DutyCycleEncoder m_encoderShoulder = new DutyCycleEncoder(0, 360, 0); 
+  private final DutyCycleEncoder m_encoderElbow = new DutyCycleEncoder(1, 360, 0);
+
   /** Creates a new ArmSubsystem. */
   public ArmSubsystem() {
     // eklemli
@@ -17,5 +31,9 @@ public class ArmSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putBoolean("Encoder 1 Connected?", m_encoderShoulder.isConnected());
+    SmartDashboard.putBoolean("Encoder 2 Connected?", m_encoderElbow.isConnected());
+    SmartDashboard.putNumber("Encoder 1:", m_encoderShoulder.get());
+    SmartDashboard.putNumber("Encoder 2:", m_encoderElbow.get());
   }
 }
