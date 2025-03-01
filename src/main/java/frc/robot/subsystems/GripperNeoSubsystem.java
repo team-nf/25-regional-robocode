7e19e304fb7204f84fc5e1c4009085b146567e1c
yehpop <smsglxyn8@gmail.com>
2025-03-01 19:21:11 +0300

    Add gripper using NEO as secondary gripper subsystem

---
+++ src/main/java/frc/robot/subsystems/GripperNeoSubsystem.java
@@ -0,0 +1,109 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.GripperConstants;
+
+
+public class GripperSubsystem extends SubsystemBase {
+ private final SparkMax m_spark = new SparkMax(GripperConstants.DEVICE_ID, MotorType.kBrushless);
+  private SparkMaxConfig motorConfig;
+  private SparkClosedLoopController sparkPID;
+
+  private final DigitalInput m_algeaSensor = new DigitalInput(2);
+  private final DigitalInput m_coralSensor = new DigitalInput(3);
+
+  /** 
+   * Creates a new GripperSubsystem. 
+   * Valla tuna ercan senden kopyaladım.
+   */
+  public GripperSubsystem() {
+      // Create SparkMAX Config Object (I hate this new abundant API.)
+      motorConfig = new SparkMaxConfig();
+  
+      /** 
+       * Configure the encoder. We are using the encoder of NEO Vortex, therefore
+       * no config is needed, yet adjusting conversion factors is needed. 
+       */
+      motorConfig.encoder
+        .positionConversionFactor(GripperConstants.POSITION_CONVERSION_FACTOR)
+        .velocityConversionFactor(GripperConstants.VELOCITY_CONVERSION_FACTOR);
+  
+      /**
+       * Configure the closed loop controller. The feedback sensor is the primary encoder.
+       * The closed loop controller will be used for velocity control.
+       */
+      motorConfig.closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        .p(GripperConstants.kP)
+        .i(GripperConstants.kI)
+        .d(GripperConstants.kD)
+        .velocityFF(GripperConstants.kV)
+        .outputRange(GripperConstants.LOW_OUT, GripperConstants.HIGH_OUT);
+  
+      motorConfig.closedLoop.maxMotion
+      // Set MAXMotion parameters for velocity control
+        .maxAcceleration(GripperConstants.MAX_ACC)
+        .maxVelocity(GripperConstants.MAX_VEL)
+        .allowedClosedLoopError(GripperConstants.ALLOWED_ERR);
+  
+      // Apply the configuration to the Spark MAX
+      m_spark.configure(motorConfig, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  
+  
+
+  
+    // Initialize telemetry
+    SmartDashboard.setDefaultNumber("Target Velocity", 0);
+
+  }
+
+  /** Used for testing */
+  //public Command controlWithTriggers(double input) {return run(() -> );}
+
+  public Command controlFromDashboard() {
+    double targetVelocity = SmartDashboard.getNumber("Target Velocity", 0);
+    return run(() -> sparkPID.setReference(targetVelocity, ControlType.kMAXMotionVelocityControl));
+  }
+
+  // Maybe use motion magic?? Test first.
+
+  public Command controlFromTargetVelocity(double targetVelocity) 
+  {return run(() -> sparkPID.setReference(targetVelocity, ControlType.kMAXMotionVelocityControl));}
+
+  public Command takeAlgae() {return run(() -> sparkPID.setReference(0.5, ControlType.kMAXMotionVelocityControl));}
+
+  public Command takeCoral() {return run(() -> sparkPID.setReference(-0.3, ControlType.kMAXMotionVelocityControl));}
+
+  public Command throwAlgae() {return run(() -> sparkPID.setReference(-0.6, ControlType.kMAXMotionVelocityControl));}
+
+  public Command throwCoral() {return run(() -> sparkPID.setReference(0.5, ControlType.kMAXMotionVelocityControl));}
+
+  public Command stop() {return run(() -> sparkPID.setReference(0.0, ControlType.kMAXMotionVelocityControl));}
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+
+    // Telemetry
+    SmartDashboard.putNumber("Current Velocity", m_spark.getAbsoluteEncoder().getVelocity());
+
+    SmartDashboard.putData("Has Coral?: ", m_coralSensor);
+    SmartDashboard.putData("Has Algea?: ", m_algeaSensor);
+  }
+}
